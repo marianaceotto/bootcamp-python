6bb3518c3339fb4c2bc5254e2bd5676a7f360c6f
marianaceotto <marianaceott@gmail.com>
2023-06-19 11:37:52 -0300

    Desafio 1

--- desafio.py
+++ desafio.py
@@ -1,22 +1,19 @@
 menu = """
-[1] Depositar
-[2] Sacar
-[3] Extrato
-[4] Sair
-=> """
+[1] Depositar / [2] Sacar / [3] Extrato / [4] Sair
+"""
 
 saldo = 0
 deposito = 0
-limite = 500
 extrato = ""
+limite = 500
 numero_saques = 0
 LIMITE_SAQUES = 3
 
 while True:
+    
+    selecione = input(menu)
 
-    opcao = input(menu)
-
-    if opcao == '1':
+    if selecione == '1':
         deposito = float(input('Quanto você quer depositar? R$'))
 
         if deposito > 0:
@@ -24,23 +21,23 @@ while True:
             print(f'Saldo: R${saldo}')
             extrato += (f'\nDepósito: R$ {deposito:.2f}')
         else:
-            print('Operação falou! Digite um valor válido!')
+            print('Digite um valor válido')
 
-    elif opcao == '2':
+    elif selecione == '2':
 
         while True:
             if numero_saques >= LIMITE_SAQUES:
-                print('Limite máximo de saques realizados por dia')
+                print('Limite máximo de saques')
                 break
 
-            saque = float(input('Quanto você quer sacar? R$'))
+            saque = float(input('Qual valor deseja sacar? R$'))
 
             if saque > saldo:
-                print('Não foi possível sacar esse valor, pois não tem saldo dísponivel!')
+                print('Não foi possível realizar essa transação, saldo indisponível')
                 break
 
             if saque >= 500:
-                print('Você só pode sacar no máximo R$500,00 reais por saque!')
+                print('Máximo R$500,00 reais por saque')
                 break
 
             if saque > 0:
@@ -54,14 +51,14 @@ while True:
                     break
             else:
                 print('Valor informado inválido!')
-    elif opcao == '3':
-        print('=================EXTRATO=================')
+    elif selecione == '3':
+        print('==============EXTRATO')
         print('Não foram realizados movimentações em sua conta.' if not extrato else extrato)
         print(f'Saldo: R$ {saldo:.2f}')
         print('=========================================')
 
-    elif opcao == '4':
-        print('Saindo...')
+    elif selecione == '4':
+        print('Aguarde...')
         break
     else:
-        print('Operação inválida, por favor digite novamente.')
+        print('Operação inválida')
